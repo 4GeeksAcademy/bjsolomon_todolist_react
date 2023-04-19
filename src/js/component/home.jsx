98b82a25c1557eb5981fca27e6a1c4d52ca7a355
Brent Solomon <55516169+kingbsolomon@users.noreply.github.com>
2023-04-19 23:15:16 +0000

    updated home.jex

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
 		<div className="text-center">
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
 			<p>
-				<img src={rigoImage} />
+				HELLO CODESPACES!
 			</p>
 			<a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working...
